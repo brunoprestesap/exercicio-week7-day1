30c700f173b521d7a8c2b083bcd8075792388795
Bruno Prestes <brunoasprestes@gmail.com>
2022-11-28 20:51:29 -0300

    Exercicio da aula

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 import express from "express";
 import * as dotenv from "dotenv";
+
+//Habilitar o server para ter variaveis de ambiente
+dotenv.config()
+
+//Instanciar o express
+const app = express()
+
+//Configurar Server Express para funcionar com JSON
+app.use(express.json())
+
+app.get("/", (req, res) => {
+
+    const welcome = "Bem vendo ao express"
+
+    return res.status(200).json(welcome)
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`App Up and running on port http://localhost:${process.env.PORT}`);
+})
